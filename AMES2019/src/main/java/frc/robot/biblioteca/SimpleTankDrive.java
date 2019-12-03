d1f783644b397995563bf95940b5ab08bc386ff7
Dhhoyt <36286620+Dhhoyt@users.noreply.github.com>
2019-12-03 14:46:14 -0700

    Update SimpleTankDrive.java

--- AMES2019/src/main/java/frc/robot/biblioteca/SimpleTankDrive.java
+++ AMES2019/src/main/java/frc/robot/biblioteca/SimpleTankDrive.java
@@ -7,6 +7,8 @@ class SimpleTankDrve extends Drive{
   private TalonSRX m_leftSlave;
   private TalonSRX m_rightMaster;
   private TalonSRX m_rightSlave;
+  int twistInv;
+  int driveInv;
   SimpleTankDrive(int leftMasterPort, int leftSlavePort, int rightMasterPort, int rightSlavePort){
     m_leftMaster = new TalonSRX(leftMasterPort);
     m_leftSlave = new TalonSRX(leftSlavePort);
@@ -14,13 +16,28 @@ class SimpleTankDrve extends Drive{
     m_rightSlave = new TalonSRX(rightSlavePort);
     m_rightSlave.follow(m_rightMaster);
     m_leftSlave.follow(m_leftMaster);
+    driveInv = 1;
     super();
   }
   @Overide
+  public void invertTwist(boolean twist);
+    if(twist == true){
+      twistInv = -1;
+    }else{
+      twistInv = 1; 
+    }
+  }
+  public void invertTwist(boolean drive);
+    if(drive == true){
+      driveInv = -1;
+    }else{
+      driveInv = 1; 
+    }
+  }
   public void gatherInfo(){}
   @Override
   public void doActions(){
-    m_leftMaster.set(m_forward - m_twist);
-    m_rightMaster.set(m_forward + m_twist);
+    m_leftMaster.set((m_forward * driveInv) - (m_twist * twistInv));
+    m_rightMaster.set((m_forward * driveInv) + (m_twist * twistInv));
   }
 }
