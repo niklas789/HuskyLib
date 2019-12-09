65f66834b8343f2a1c99909331bac04933d45f6b
Dhhoyt <36286620+Dhhoyt@users.noreply.github.com>
2019-12-09 14:57:09 -0700

    Update VictorSPX.java

--- AMES2019/src/main/java/frc/robot/biblioteca/VictorSPX.java
+++ AMES2019/src/main/java/frc/robot/biblioteca/VictorSPX.java
@@ -1,6 +1,6 @@
 package frc.robot.biblioteca;
 import frc.robot.biblioteca.*;
-public class VictorSPX extends motor {
+public class VictorSPX extends Motor {
     public VictorSPX(int port) {
         super(port);
     }
