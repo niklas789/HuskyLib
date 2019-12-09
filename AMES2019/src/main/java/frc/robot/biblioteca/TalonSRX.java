9c4a650424c865090d27c0ba89108b7015156ea8
Dhhoyt <36286620+Dhhoyt@users.noreply.github.com>
2019-12-09 14:46:22 -0700

    Update TalonSRX.java

--- AMES2019/src/main/java/frc/robot/biblioteca/TalonSRX.java
+++ AMES2019/src/main/java/frc/robot/biblioteca/TalonSRX.java
@@ -1,7 +1,7 @@
 package frc.robot.biblioteca;
 import frc.robot.biblioteca.*;
 public class TalonSRX extends Motor {
-    public talonSRX(int port) {
+    public TalonSRX(int port) {
         super(port);
     }
 }
