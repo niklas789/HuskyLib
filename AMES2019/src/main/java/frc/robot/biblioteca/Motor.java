272f1bd74c31546b19cc8dfa9b8f513076a83a71
Dhhoyt <36286620+Dhhoyt@users.noreply.github.com>
2019-12-11 09:38:53 -0700

    Update Motor.java

--- AMES2019/src/main/java/frc/robot/biblioteca/Motor.java
+++ AMES2019/src/main/java/frc/robot/biblioteca/Motor.java
@@ -1,13 +1,21 @@
 package frc.robot.biblioteca;
 import frc.robot.biblioteca.*;
 class Motor extends AnalogOutput {
+    private double m_speed;
+    private double m_realSpeed;
     public Motor(int port) {
         super(1, -1);
     }
     public void set(double value) {
-
+        m_Speed = value;
     }
     public void follow(Motor motor) {
 
+    }
+    public double getTargetSpeed(){
+        return m_speed;
+    }
+    public double getRealSpeed(){
+        return m_realSpeed;
     }
 }
